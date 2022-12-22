341e72a525ef0376c5c98799d8cba9039612ead0
KobzarAnatolii <TohaPirat@gmail.com>
2022-12-22 18:51:10 +0200

    done task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,17 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-// var throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const CURRENTTIME_LOCAL = 'videoplayer-current-time';
+const KEY_LOCAL = 'videoplayer-current-time';
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay(currentTime) {
-  localStorage.setItem(CURRENTTIME_LOCAL, JSON.stringify(currentTime));
+  localStorage.setItem(KEY_LOCAL, JSON.stringify(currentTime));
 }
 
-const currentTime = JSON.parse(localStorage.getItem(CURRENTTIME_LOCAL));
+const currentTime = JSON.parse(localStorage.getItem(KEY_LOCAL));
 const { seconds } = currentTime;
 
 player.setCurrentTime(seconds);
